0aac6406e23e534e5909f16d2579349114eeb17b
BjoernBC <bbc55755@gmail.com>
2018-12-13 16:19:19 +0100

    Kiks

--- de andres/alexander/index.php
+++ de andres/alexander/index.php
@@ -13,162 +13,198 @@ and open the template in the editor.
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
     </head>
     <body>
-        
-        <div id="body">
-            <header>
-                <nav id="topNav">
+        <header>
+            <nav id="topNav">
+                <div class="wrap">
                     <ul>
                         <li>
-                            <a href="" style="align-self: flex-start;">Home</a>
-                        </li>
-                        <li>
-                            <a href="">Kontakt</a>
-                        </li>
-                        <li>
-                            <a href=""><i class="fab fa-facebook-square"></i></a>
-                        </li>
-                        <li>
-                            <a href=""><i class="fab fa-instagram"></i></a>
+                            <a href="#">Home</a>
                         </li>
                     </ul>
-                </nav>
-                <section id="ungeNav">
-                    <nav id="ungeNavContainer" class="wrap">
-                        <div class="navBtn">
-                            <a href=""><i class="fas fa-angle-left"></i></a>
-                        </div>
-                        <div class="ungeSliderContent">
+                    <ul>
+                        <li><a href="#">Kontakt</a></li>
+                        <li><a href="#"><i class="fab fa-facebook-square"></i></a></li>
+                        <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                        <li><a href="#"><i class="fab fa-linkedin"></i></a></li>
+                        <li><a href="#"><i class="fab fa-snapchat"></i></a></li>
+                    </ul>
+                </div>
+            </nav>
+            <section id="ungeNav">
+                <nav id="ungeNavContainer" class="wrap">
+                    <div class="navBtn left">
+                        <a href="#"><i class="fas fa-angle-left"></i></a>
+                    </div>
+                    <div class="ungeSliderContent">
                         <div class="slider navSlider">
-                            <a href="">
+                            <a href="#">
                                 <img src="http://pngimg.com/uploads/football/football_PNG52781.png">
                                 <div class="catBox">
                                     <p>Sport</p>
                                 </div>
                             </a>
-                            <a href="">
-                                <img src="http://pngimg.com/uploads/football/football_PNG52781.png">
+                            <a href="#">
+                                <img src="controller.png">
                                 <div class="catBox">
-                                    <p>Sport</p>
+                                    <p>Spil</p>
                                 </div>
                             </a>
-                            <a href="">
+                            <a href="#">
                                 <img src="http://pngimg.com/uploads/football/football_PNG52781.png">
                                 <div class="catBox">
-                                    <p>Sport</p>
+                                    <p>Kategori</p>
                                 </div>
                             </a>
-                            <a href="">
+                            <a href="#">
                                 <img src="http://pngimg.com/uploads/football/football_PNG52781.png">
                                 <div class="catBox">
-                                    <p>Sport</p>
+                                    <p>Kategori</p>
+                                </div>
+                            </a>
+                            <a href="#">
+                                <img src="http://pngimg.com/uploads/football/football_PNG52781.png">
+                                <div class="catBox">
+                                    <p>Kategori</p>
+                                </div>
+                            </a>
+                            <a href="#">
+                                <img src="http://pngimg.com/uploads/football/football_PNG52781.png">
+                                <div class="catBox">
+                                    <p>Kategori</p>
+                                </div>
+                            </a>
+                            <a href="#">
+                                <img src="http://pngimg.com/uploads/football/football_PNG52781.png">
+                                <div class="catBox">
+                                    <p>Kategori</p>
+                                </div>
+                            </a>
+                            <a href="#">
+                                <img src="http://pngimg.com/uploads/football/football_PNG52781.png">
+                                <div class="catBox">
+                                    <p>Kategori</p>
                                 </div>
                             </a>
                         </div>
-                        </div>
-                        <div class="navBtn">
-                            <a href=""><i class="fas fa-angle-right"></i></a>
-                        </div>
-                    </nav>
-                </section>
-            </header>
-            <section id="primary ungePrimary">
-                <section class="Headline ungeHeadline container wrap">
-                    <article>
-                        <a href="">
-                        <div class="headlineTitle">
-                            <div class="headlineBox">
-                                <h1>Title.</h1>
-                            </div>
-                        </div>
-                        <img src="https://www.incimages.com/uploaded_files/image/1940x900/getty_818918912_2000133120009280241_360639.jpg">
-                        </a>
-                    </article>
-                    <article>
-                        <div class="headlineTitle">
-                            <div class="headlineBox">
-                                <h1>Title.</h1>
-                            </div>
-                        </div>
-                        <img src="https://www.incimages.com/uploaded_files/image/1940x900/getty_818918912_2000133120009280241_360639.jpg">
-                        
-                    </article>
-                </section>
-                <section class="slider ungeNewsSlider">
-                    <div class="navBtn">
-                        <a href=""><i class="fas fa-angle-left"></i></a>
                     </div>
-                    <div class="newsThumbnail">
-                        <article>
-                            <a href=""><img src="https://www.incimages.com/uploaded_files/image/1940x900/getty_818918912_2000133120009280241_360639.jpg"><div class="newsTitleBox"><h1>Title.</h1></div></a>
-                        </article>
-                        <article>
-                            <a href=""><img src="https://www.incimages.com/uploaded_files/image/1940x900/getty_818918912_2000133120009280241_360639.jpg"><div class="newsTitleBox"><h1>Title.</h1></div></a>
-                        </article>
-                        <article>
-                            <a href=""><img src="https://www.incimages.com/uploaded_files/image/1940x900/getty_818918912_2000133120009280241_360639.jpg"><div class="newsTitleBox"><h1>Title.</h1></div></a>
-                        </article>
-                        <article>
-                            <a href=""><img src="https://www.incimages.com/uploaded_files/image/1940x900/getty_818918912_2000133120009280241_360639.jpg"><div class="newsTitleBox"><h1>Title.</h1></div></a>
-                        </article>
-                        <article>
-                            <a href=""><img src="https://www.incimages.com/uploaded_files/image/1940x900/getty_818918912_2000133120009280241_360639.jpg"><div class="newsTitleBox"><h1>Title.</h1></div></a>
-                        </article>
-                        <article>
-                            <a href=""><img src="https://www.incimages.com/uploaded_files/image/1940x900/getty_818918912_2000133120009280241_360639.jpg"><div class="newsTitleBox"><h1>Title.</h1></div></a>
-                        </article>
-                        <article>
-                            <a href=""><img src="https://www.incimages.com/uploaded_files/image/1940x900/getty_818918912_2000133120009280241_360639.jpg"><div class="newsTitleBox"><h1>Title.</h1></div></a>
-                        </article>
-                        <article>
-                            <a href=""><img src="https://www.incimages.com/uploaded_files/image/1940x900/getty_818918912_2000133120009280241_360639.jpg"><div class="newsTitleBox"><h1>Title.</h1></div></a>
-                        </article>
-                        <article>
-                            <a href=""><img src="https://www.incimages.com/uploaded_files/image/1940x900/getty_818918912_2000133120009280241_360639.jpg"><div class="newsTitleBox"><h1>Title.</h1></div></a>
-                        </article>
-                        <article>
-                            <a href=""><img src="https://www.incimages.com/uploaded_files/image/1940x900/getty_818918912_2000133120009280241_360639.jpg"><div class="newsTitleBox"><h1>Title.</h1></div></a>
-                        </article>
+                    <div class="navBtn right">
+                        <a href="#"><i class="fas fa-angle-right"></i></a>
                     </div>
-                </section>
-                <section class="articleEnv wrap">
-                    <article>
-                        <a href="">
-                            <img src="https://www.incimages.com/uploaded_files/image/1940x900/getty_818918912_2000133120009280241_360639.jpg">
-                            <h1>Title.</h1>
-                        </a>
-                    </article>
-                    <article>
-                        <a href="">
-                            <img src="https://www.incimages.com/uploaded_files/image/1940x900/getty_818918912_2000133120009280241_360639.jpg">
-                            <h1>Donald Trump har nu igen truet danmark med missiler.</h1>
-                        </a>
-                    </article>
-                    <article>
-                        <a href="">
-                            <img src="https://www.incimages.com/uploaded_files/image/1940x900/getty_818918912_2000133120009280241_360639.jpg">
-                            <h1>Title.</h1>
-                        </a>
-                    </article>
-                    <article>
-                        <a href="">
-                            <img src="https://www.incimages.com/uploaded_files/image/1940x900/getty_818918912_2000133120009280241_360639.jpg">
-                            <h1>Title.</h1>
-                        </a>
-                    </article>
-                    <article>
-                        <a href="">
-                            <img src="https://www.incimages.com/uploaded_files/image/1940x900/getty_818918912_2000133120009280241_360639.jpg">
-                            <h1>Title.</h1>
-                        </a>
-                    </article>
-                    <article>
-                        <a href="">
-                            <img src="https://www.incimages.com/uploaded_files/image/1940x900/getty_818918912_2000133120009280241_360639.jpg">
-                            <h1>Title.</h1>
-                        </a>
-                    </article>
-                </section>
+                </nav>
             </section>
-        </div>
+        </header>
+        <main id="primary ungePrimary">
+            <section class="Headline ungeHeadline container wrap">
+                <article>
+                    <a href="#">
+                    <div class="headlineTitle">
+                        <div class="headlineBox">
+                            <h1>Title.</h1>
+                        </div>
+                    </div>
+                    <img src="https://via.placeholder.com/400x400.png?text=Placeholder">
+                    </a>
+                </article>
+                <article>
+                    <div class="headlineTitle">
+                        <div class="headlineBox">
+                            <h1>Title.</h1>
+                        </div>
+                    </div>
+                    <img src="https://via.placeholder.com/400x400.png?text=Placeholder">
+                    
+                </article>
+            </section>
+            <section class="slider ungeNewsSlider">
+                <div class="navBtn left">
+                    <a href="#"><i class="fas fa-angle-left"></i></a>
+                </div>
+                <div class="newsThumbnail">
+                    <article>
+                        <a href="#"><img src="https://via.placeholder.com/400x400.png?text=Placeholder"><div class="newsTitleBox"><h1>Title.</h1></div></a>
+                    </article>
+                    <article>
+                        <a href="#"><img src="https://via.placeholder.com/400x400.png?text=Placeholder"><div class="newsTitleBox"><h1>Title.</h1></div></a>
+                    </article>
+                    <article>
+                        <a href="#"><img src="https://via.placeholder.com/400x400.png?text=Placeholder"><div class="newsTitleBox"><h1>Title.</h1></div></a>
+                    </article>
+                    <article>
+                        <a href="#"><img src="https://via.placeholder.com/400x400.png?text=Placeholder"><div class="newsTitleBox"><h1>Title.</h1></div></a>
+                    </article>
+                    <article>
+                        <a href="#"><img src="https://via.placeholder.com/400x400.png?text=Placeholder"><div class="newsTitleBox"><h1>Title.</h1></div></a>
+                    </article>
+                    <article>
+                        <a href="#"><img src="https://via.placeholder.com/400x400.png?text=Placeholder"><div class="newsTitleBox"><h1>Title.</h1></div></a>
+                    </article>
+                    <article>
+                        <a href="#"><img src="https://via.placeholder.com/400x400.png?text=Placeholder"><div class="newsTitleBox"><h1>Title.</h1></div></a>
+                    </article>
+                    <article>
+                        <a href="#"><img src="https://via.placeholder.com/400x400.png?text=Placeholder"><div class="newsTitleBox"><h1>Title.</h1></div></a>
+                    </article>
+                    <article>
+                        <a href="#"><img src="https://via.placeholder.com/400x400.png?text=Placeholder"><div class="newsTitleBox"><h1>Title.</h1></div></a>
+                    </article>
+                    <article>
+                        <a href="#"><img src="https://via.placeholder.com/400x400.png?text=Placeholder"><div class="newsTitleBox"><h1>Title.</h1></div></a>
+                    </article>
+                </div>
+                <div class="navBtn right">
+                    <a href="#"><i class="fas fa-angle-right"></i></a>
+                </div>
+            </section>
+            <section class="articleEnv wrap">
+                <article>
+                    <a href="#">
+                        <img src="https://via.placeholder.com/400x400.png?text=Placeholder">
+                        <h1>Title.</h1>
+                    </a>
+                </article>
+                <article>
+                    <a href="#">
+                        <img src="https://via.placeholder.com/400x400.png?text=Placeholder">
+                        <h1>Title.</h1>
+                    </a>
+                </article>
+                <article>
+                    <a href="#">
+                        <img src="https://via.placeholder.com/400x400.png?text=Placeholder">
+                        <h1>Title.</h1>
+                    </a>
+                </article>
+                <article>
+                    <a href="#">
+                        <img src="https://via.placeholder.com/400x400.png?text=Placeholder">
+                        <h1>Title.</h1>
+                    </a>
+                </article>
+                <article>
+                    <a href="#">
+                        <img src="https://via.placeholder.com/400x400.png?text=Placeholder">
+                        <h1>Title.</h1>
+                    </a>
+                </article>
+                <article>
+                    <a href="#">
+                        <img src="https://via.placeholder.com/400x400.png?text=Placeholder">
+                        <h1>Title.</h1>
+                    </a>
+                </article>
+            </section>
+        </main>
+        <footer>
+            <div class="some wrap">
+                <img src="logo.png">
+                <ul>
+                    <li>FØLG OS:</li>
+                    <li><a href="#"><i class="fab fa-facebook-square"></i></a></li>
+                    <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                    <li><a href="#"><i class="fab fa-linkedin"></i></a></li>
+                    <li><a href="#"><i class="fab fa-snapchat"></i></a></li>
+                </ul>
+            </div>
+        </footer>
     </body>
 </html>
