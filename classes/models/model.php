64cdc512d022689e7e5a9e7aa9ae55ee9187db3e
Chris <christoffer_knudsen@live.dk>
2018-12-14 12:41:06 +0100

    words

--- classes/models/model.php
+++ classes/models/model.php
@@ -2,8 +2,6 @@
 require_once rootPath . 'classes/models/modelif.php';
 
 abstract class Model implements ModelIf {
-
-
     public function __construct() {
 
     }
--- index.php
+++ index.php
@@ -5,20 +5,4 @@ define('rootPath', '');
 require_once('classes/controllers/controller.php');
 $cont = new Controller();
 $cont->drawPage('frontpage');
-/*
-require_once('classes/models/newsmodel.php');
-require_once('classes/views/newsview.php');
-$newsM =  new NewsModel('https://www.jv.dk/rss/nyheder');
-$newsMBT =  new NewsModel('https://www.berlingske.dk/content/rss');
-$newsM->merge($newsMBT);
-$newsM->sort();
-$articles = $newsM->getArticles();
-foreach($articles as $article){
-	$viewObj = new NewsView($article);
-	echo $viewObj->buildStandard();
-}
-echo "<pre>";
-print_r($articles);
-echo "</pre>";
-*/
 ?>
