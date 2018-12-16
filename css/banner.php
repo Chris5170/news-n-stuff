971a8c974b913a1abdf063b0338659de0b6e1467
Christoffer H. Knudsen <christoffer_knudsen@live.dk>
2018-12-16 18:15:08 +0100

    Update banner.php

--- css/banner.php
+++ css/banner.php
@@ -1,7 +1,8 @@
 .body-front {
-    margin-top: 5em;
+    padding-top: 5em;
     /*height: 30em;*/
     min-height: 30em;
+    background-color: <?php echo self::getColor('.body-front'); ?>;
     background-image: url("https://meemiq.com/wp-content/uploads/2018/10/Bizmiq_Hvid-%C3%94%C3%87%C3%B4-transparent.png");
     background-position: center center;
     background-size: contain; 
